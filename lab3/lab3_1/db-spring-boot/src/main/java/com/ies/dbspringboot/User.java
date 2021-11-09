94bf05c1ec045a65e0baec4ff95d856b30079d28
TheScorpoi <sobral@ua.pt>
2021-11-09 15:37:49 +0000

    lab3_1 completed

---
+++ lab3/lab3_1/db-spring-boot/src/main/java/com/ies/dbspringboot/User.java
@@ -0,0 +1,71 @@
+package com.ies.dbspringboot;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    
+    
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    @NotBlank(message = "Phone is mandatory")
+    private String phone;
+
+    public User() {}
+
+    public User(Long id, String name, String email, String phone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", id=" + id + ", name=" + name + ", phone=" + phone + "]";
+    }
+}
---
+++ lab3/lab3_1/db-spring-boot/src/test/java/com/ies/dbspringboot/DbSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ies.dbspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
